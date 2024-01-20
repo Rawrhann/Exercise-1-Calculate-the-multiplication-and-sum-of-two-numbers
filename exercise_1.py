44848dd544ee6baa9c2e6c8b7000d5f4bd2ef564
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-20 17:38:20 +0800

    Function

--- exercise_1.py
+++ exercise_1.py
@@ -1,6 +1,14 @@
 #Exercise 1: Calculate the multiplication and sum of two numbers
 #Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
 
+#function
+def product_or_sum(first_given_number, second_given_number):
+    product = first_given_number * second_given_number
+    if product <= 1000:
+        return product
+    else:
+        return (first_given_number + second_given_number)
+
 #Given 1
 given1_number1 = 20
 given1_number2 = 30
