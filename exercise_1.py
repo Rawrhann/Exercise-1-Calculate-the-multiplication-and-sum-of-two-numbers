c5e62f91cfecdb7c3075eb0de30a8c978b825e2d
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-20 17:14:20 +0800

    Given

--- exercise_1.py
+++ exercise_1.py
@@ -1,3 +1,11 @@
 #Exercise 1: Calculate the multiplication and sum of two numbers
 #Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
 
+#Given 1
+given1_number1 = 20
+given1_number2 = 30
+
+#Given 2
+given2_number1 = 40
+given2_number2 = 30
+
